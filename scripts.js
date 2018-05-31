0d011cd1cb0691043513ce0b5f2758eff987b560
magda <magdalena.aniol@gmail.com>
2018-05-31 12:44:59 +0200

    optimized and improved editing

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*global variables - I put them here because different funcions need access to them*/
-var finalResultUser = 0;
+var finalResultPlayer = 0;
 var finalResultComputer = 0;
 var maxRounds = 0;
 var roundsCompleted = 0;
@@ -9,116 +9,83 @@ var result = document.getElementById('result');
 var output = document.getElementById('output');
 
 /*aux functions*/
-var getComputerMove = function (){
-	var randomResult = Math.floor((Math.random() * 3) + 1);
-	switch(randomResult) {
-		case 1:
-			return "paper";
-		case 2:
-			return"rock";
-		case 3:
-			return "scissors";
+var getComputerMove = function () {
+	var possiblePicks = ['paper', 'rock', 'scissors'];
+	return possiblePicks[Math.floor(Math.random() * 3)];
+};
+
+var disableGameButtons = function (isDisabled) {
+	buttonRock.disabled = isDisabled;
+	buttonPaper.disabled = isDisabled;
+	buttonScissors.disabled = isDisabled;
+};
+
+var publishResults = function(winnerIs, computerMove, playerMove) {
+	if (winnerIs === 'none') {
+		output.innerHTML = 'It\'s a DRAW!<br>';
+	} else if (winnerIs === 'player') {
+		output.innerHTML = 'You WIN! You played ' + playerMove + ' and computer played ' + computerMove + '<br>';
+		finalResultPlayer++;
+		roundsCompleted++;
+	} else {
+		output.innerHTML = 'You LOSE! You played ' + playerMove + ' and computer played ' + computerMove + '<br>';
+		finalResultComputer++;
+		roundsCompleted++;
 	}
-}
-
-var disableGameButtons = function (bool) {
-	document.getElementById("rock").disabled = bool;
-	document.getElementById("paper").disabled = bool;
-	document.getElementById("scissors").disabled = bool;
-}
-
-
-var publishResults = function(draw, userWinner, userMove, computerMove, gameOver) {
-	if (draw) { output.innerHTML = "It's a DRAW!<br>";}
-	else if (userWinner) {
-		output.innerHTML = "You WIN! You played " + userMove + " and computer played " + computerMove + "<br>";
-		finalResultUser += 1;
-	}
-  else {
-  	output.innerHTML = "You LOSE! You played " + userMove + " and computer played " + computerMove + "<br>";
-  	finalResultComputer += 1;
-  }
-  result.innerHTML = finalResultUser + " : " + finalResultComputer + "<br>";
-  if (gameOver){
-  	if (finalResultUser > finalResultComputer) {
-  		result.insertAdjacentHTML('beforeend',"YOU WON THE ENTIRE GAME!<br>");
-  	}
-  	else if (finalResultUser < finalResultComputer) {
-  		result.insertAdjacentHTML('beforeend',"YOU LOSER!<br>");
-  	}
-		else {
-			result.insertAdjacentHTML('beforeend', "YOU'RE BOTH WINNERS! IT'S A DRAW!<br>");
-		}
-		result.insertAdjacentHTML('beforeend',"Game over, please press the new game button!");
+	result.innerHTML = finalResultPlayer + ' : ' + finalResultComputer + '<br>';
+	if (roundsCompleted == maxRounds) {
 		disableGameButtons(true);
-  }
-}
+		if (finalResultPlayer > finalResultComputer) {
+			result.insertAdjacentHTML('beforeend','YOU WON THE ENTIRE GAME!<br>');
+		} else if (finalResultPlayer < finalResultComputer) {
+			result.insertAdjacentHTML('beforeend','YOU LOSER!<br>');
+		} else {
+			result.insertAdjacentHTML('beforeend', 'YOU\'RE BOTH WINNERS! IT\'S A DRAW!<br>');
+		}
+	roundsCompleted = 0;
+	finalResultComputer = 0;
+	finalResultPlayer = 0;
+	result.insertAdjacentHTML('beforeend',"Game over, please press the new game button!");
+	}
+};
 
 /*new game button handler*/
 function promptNewGame() {
-	result.innerHTML = "";
-	output.innerHTML = "";
+	result.innerHTML = '';
+	output.innerHTML = '';
 	var roundCounter = document.getElementById('roundCounter');
-	maxRounds = prompt("Please provide the number of rounds in the match");
-	if (maxRounds === null) {
+	maxRounds = prompt('Please provide the number of rounds in the match');
+	if (maxRounds === null || maxRounds === '') {
 		disableGameButtons(true);
-		roundCounter.innerHTML = "Please press the new game button and choose the number of rounds!";
-	}
-	else if (maxRounds === "") {
+		roundCounter.innerHTML = 'Please press the new game button and choose the number of rounds!';
+	} else if (isNaN(maxRounds)) {
 		disableGameButtons(true);
-		roundCounter.innerHTML = "Please press the new game button and choose the number of rounds!";
-	}
-	else if (isNaN(maxRounds)){
-		disableGameButtons(true);
-		roundCounter.innerHTML = "Invalid input! Please provide a number.";
-	}
-	else {
-		roundCounter.innerHTML = "This match has " + maxRounds + " rounds!";
-		finalResultComputer=0;
-		finalResultComputer=0;
+		roundCounter.innerHTML = 'Invalid input! Please provide a number.';
+	} else {
+		roundCounter.innerHTML = 'This match has ' + maxRounds + ' rounds!';
 		disableGameButtons(false);
 	}
-}
+};
 
 /*main*/
-var playerMove = function(move) {
-	var userWinner = false;
-	var draw = false;
-	var gameOver = false;
+var playerMove = function(playerMove) {
 	var computerMove =  getComputerMove();
-	switch(computerMove) {
-		case 'paper':
-	  	if (move == 'paper') { draw = true;}
-	  	else if (move == 'rock') {userWinner = false;}
-	  	else { userWinner = true;}
-	    break;
-	  case 'rock':
-	  	if (move == 'rock') { draw = true;}
-	  	else if (move == 'paper') {userWinner = true;}
-	  	else { userWinner = false;}
-	    break;
-	  case 'scissors':
-	  	if (move == 'scissors') { draw = true;}
-	  	else if (move == 'stone') {userWinner = true;}
-	  	else { userWinner = false;}
-	  }
-	if(!draw) {
-		roundsCompleted += 1;
+	var winnerIs = 'player';
+	if (computerMove === 'scissors' && playerMove === 'paper' ||
+		  computerMove === 'rock' && playerMove === 'scissors' ||
+		  computerMove === 'paper' && playerMove === 'rock' ) {
+		winnerIs = 'computer';
+	} else if (computerMove === playerMove) {
+		winnerIs = 'none';
 	}
-	if (roundsCompleted == maxRounds)
-	{
-		gameOver = true;
-		roundsCompleted=0;
-		disableGameButtons(true);
-	}
-	publishResults(draw, userWinner, move, computerMove, gameOver);
-}
+	publishResults(winnerIs, playerMove, computerMove);
+};
 
 /*event listeners*/
 
-var buttonRock = document.getElementById("rock");
-var buttonPaper = document.getElementById("paper");
-var buttonScissors = document.getElementById("scissors");
+var buttonRock = document.getElementById('rock');
+var buttonPaper = document.getElementById('paper');
+var buttonScissors = document.getElementById('scissors');
 
 
 buttonRock.addEventListener('click', function () { playerMove('rock') });
